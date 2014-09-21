cba6193fa1d8b30b394ee2a7ef642169155057c3
anmorales <andres.me@gmail.com>
2014-09-21 11:18:26 -0600

    updated code

--- chord.erl
+++ chord.erl
@@ -1,18 +1,19 @@
 %%%-------------------------------------------------------------------
-%%% File    : Chord.erl
-%%% Author  : Andres Morales - 201016752
-%%% Description :  I Progra BDA - II-2014
+%%% File    : chord.erl
+%%% Author  : JCastro <jose.r.castro@gmail.com>
+%%% Description : 
+%%%
+%%% Created : 28 Aug 2014 by JCastro <>
 %%%-------------------------------------------------------------------
 -module(chord).
 
 -behaviour(gen_server).
 
--define(M, 10).
--define(N, 1024).
 %% API
--export([start/4,join_ring/5,add_key/3,del_key/2,get_value/2]).
+-export([start/4,join_ring/4, get_state/1]).
 
--export([find_successor/2,get_fingerTable/1,stabilize/1,fix_fingers/1,check_pred/1,notify/2]).
+-export([find_successor/2,get_fingerTable/1,stabilize/1,fix_fingers/1,check_pred/1,notify/2,add_key/3,
+	 get_value/2,del_key/2]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -35,69 +36,64 @@
 %%--------------------------------------------------------------------
 start(Name, M, TicMin, TicMax) ->
     Timer = spawn(timer,clock,[Name, TicMin, TicMax]),
-    gen_server:start_link({local, Name}, ?MODULE, {new_ring, Name, Timer, M, nil}, []).
+    gen_server:start_link({local, Name}, ?MODULE, {new_ring, Name, Timer, M}, []).
     
-join_ring(Name, M, Other, TicMin, TicMax) ->
+join_ring(Name, Other, TicMin, TicMax) ->
     Timer = spawn(timer,clock,[Name, TicMin, TicMax]),
-    gen_server:start_link({local, Name}, ?MODULE, {join_ring, Name, Timer, Other, M}, []).
+    gen_server:start_link({local, Name}, ?MODULE, {join_ring, Name, Timer, Other}, []).
 
 find_successor(Name, Id) ->
-	io:format("find_successor~n"),
     gen_server:cast(Name, {find_successor, self(), Id}),
     receive
-		{find_successor, Succ} -> Succ
-		after 1000 -> Id
+	{find_successor, Succ} -> Succ
     end.
- 
-    
-get_pred(Name, Id) -> 
-	io:format("get_pred~n"),
-	gen_server:cast(Name, {get_pred, self(), Id}),
+
+get_server(Name) ->
+	gen_server:cast(Name, {get_server, self()}),
 	receive
-		{get_pred, Pred} -> Pred
+		{get_server, State} -> 
+			State
+	end.
+	
+is_pred_alive(Name) ->
+	gen_server:cast(Name, {check_alive, self()}),
+	receive
+		{alive, _} -> 
+			alive
+		after 1000 -> death
+	end.
+	
+get_state(Name) ->
+	gen_server:cast(Name, {get_server, self()}),
+	receive
+		{get_server, State} -> 
+			io:format("==========STATE==========~n"),
+			io:format("Name: ~w~n", [State#state.id#id.name]),
+			io:format("Hash: ~w~n", [State#state.id#id.hash]),
+			io:format("M: ~w~n", [State#state.m]),
+			io:format("N: ~w~n", [State#state.n]),
+			io:format("Succ: ~w~n", [State#state.succ]),
+			io:format("Pred: ~w~n", [State#state.pred]),
+			io:format("Next: ~w~n", [State#state.next]),
+			io:format("==========STATE==========~n")
 	end.
 
-add_key(Name, Key, Value) ->
-	KeyHash = erlang:phash2(Key) rem ?N,
-	Succ = find_successor(Name, #id{name=Name, hash=erlang:phash2(Name) rem ?N}),
-	if KeyHash > ?N, KeyHash < Succ#id.hash -> 
-		put({Name, Key}, Value);
-	KeyHash < ?N ->
-		put({Name, Key}, Value);
-	true -> 
-		gen_server:cast(Succ#id.name, {add_key, Name, Key, Value})
-	end,
-	ok.
+add_key(_Name, _Key, _Value) ->
+    io:format("add_key not implemented yet~n"),
+    not_implemented.
 
-get_value(Name, Key) ->
-    KeyHash = erlang:phash2(Key) rem ?N,
-    Succ = find_successor(Name, #id{name=Name, hash=erlang:phash2(Name) rem ?N}),
-	if KeyHash > ?N, KeyHash < Succ#id.hash -> 
-		io:format("Key: ~w, Value: ~w", [Key, get({Name, Key})]);
-	KeyHash < ?N ->
-		io:format("Key: ~w, Value: ~w", [Key, get({Name, Key})]);
-	true -> 
-		gen_server:cast(Succ#id.name, {get_key, Name, Key}) 
-	end,
-    ok.
+get_value(_Name, _Key) ->
+    io:format("get_value not implemented yet~n"),
+    not_implemented.
 
-del_key(Name, Key) ->
-    KeyHash = erlang:phash2(Key) rem ?N,
-    Succ = find_successor(Name, #id{name=Name, hash=erlang:phash2(Name) rem ?N}),
-	if KeyHash > ?N, KeyHash < Succ#id.hash -> 
-		erase({Name, Key});
-	KeyHash < ?N -> 
-		erase({Name, Key});
-	true -> 
-		gen_server:cast(Succ#id.name, {del_key, Name, Key}) 
-	end,
-    ok.
+del_key(_Name, _Key) ->
+    io:format("del_key not implemented yet~n"),
+    not_implemented.
 
 
 stabilize  (Name) -> gen_server:cast(Name, stabilize).
 fix_fingers(Name) -> gen_server:cast(Name, fix_fingers).
 check_pred (Name) -> gen_server:cast(Name, check_pred).
-
 
 notify(Name, Pred) -> gen_server:cast(Name, {notify, Pred}).
 
@@ -116,41 +112,42 @@ get_fingerTable(Name) ->
 %% Description: Initiates the server
 %%--------------------------------------------------------------------
 
-init({Ring, Name, Timer, M, M2}) ->
-	case Ring of
+init({Type, Name, Timer, M_Other}) ->
+	case Type of 
 	new_ring ->
+		N = pot2(M_Other),
+		Me = #id{name=Name, hash=erlang:phash2(Name) rem N},
+		put({finger,1}, Me),
+		createFingerTable(2, M_Other),
+		{ok, 
+			#state{
+				id    = Me,
+				m     = M_Other,
+				n     = N,
+				succ  = Me,
+				pred  = nil,
+				timer = Timer,
+				next  = 1
+			}
+		};
+	join_ring ->
+		Other = get_server(M_Other),
+		M = Other#state.m,
 		N = pot2(M),
 		Me = #id{name=Name, hash=erlang:phash2(Name) rem N},
-		put({finger,1}, Me),
-		createFingerTable(2,M),
+		put({finger, 1}, Me),
+		createFingerTable(2, M),
 		{ok, 
-		 #state{
-		   id    = Me,
-		   m     = M,
-		   n     = N,
-		   succ  = Me,
-		   pred  = nil,
-		   timer = Timer,
-		   next  = 1
-		  }
-		};
-    join_ring ->
-		N = pot2(M2),
-		Me = #id{name=Name, hash=erlang:phash2(Name) rem N},
-		N1 = #id{name=M, hash=erlang:phash2(M) rem N},
-		put({finger,1}, Me),
-		createFingerTable(2,M2),
-		{ok,
-		 #state{
-		   id    = Me,
-		   m     = M2,
-		   n     = N,
-		   succ  = find_successor(M, N1),
-		   pred  = nil,
-		   timer = Timer,
-		   next  = 1
-		  }
-		} 
+			#state{
+				id    = Me,
+				m     = M,
+				n     = N,
+				succ  = find_successor(M_Other, Other#state.id),
+				pred  = nil,
+				timer = Timer,
+				next  = 1
+			}
+		}
 	end.
 
 %%--------------------------------------------------------------------
@@ -165,6 +162,7 @@ init({Ring, Name, Timer, M, M2}) ->
 handle_call(get_fingerTable, _From, State) ->
     Reply = get_fingerTable(1,State#state.m),
     {reply, Reply, State};
+
 handle_call(Request, _From, State) ->
     io:format("request ~w NOT implemented by ~w~n",[Request,State#state.id#id.name]),
     Reply = ok,
@@ -177,70 +175,96 @@ handle_call(Request, _From, State) ->
 %% Description: Handling cast messages
 %%--------------------------------------------------------------------
 handle_cast({find_successor, Proc, Id}, State) ->
-	io:format("handle_cast~n"),
     find_succ(Proc, Id, State),
     {noreply, State};
-
     
-handle_cast({get_pred, Proc, Id}, State) -> 
-	io:format("get pred for: ~w ~n", [State#state.id]),
-	get_pre(Proc, Id, State),
+handle_cast({get_server, Proc}, State) ->
+    get_serv(Proc, State),
+    {noreply, State};
+    
+handle_cast({check_alive, Proc}, State) -> 
+	check_alive_status(Proc, State),
 	{noreply, State};
-    
-handle_cast(stabilize, State) -> 
-	io:format("stabilize:~w ~n",[State#state.id#id.name]),
-	io:format("succ:~w ~n",[State#state.succ#id.name]),
-	if
-		State#state.succ == State#state.id ->
-			io:format("succ.pred = nil ~n"),
-			{noreply, State};
-		true -> 
-			X = get_pred(State#state.succ#id.name, State#state.id),
-			SuccPred = X#state.pred,
-			case SuccPred of
-				nil -> io:format("succ.pred = null ~n");
-				_-> io:format("succ.pred = ~w ~n", SuccPred#id.name)
-			end,
-			{noreply, State}
-	end;
 
-handle_cast(fix_fingers, State) -> io:format("fix_fingers: ~w ~n", [State#state.id]), 
-	NewState = State#state{next = State#state.next + 1},
-	if
-		NewState#state.next > State#state.m ->
-			NewState2 = State#state{next = 1},
-			{noreply, NewState2};
+handle_cast(stabilize,   State) -> 
+	io:format("~w stabilize~n",[State#state.id]),
+	TempSucc = State#state.succ,
+	if TempSucc == State#state.id ->
+			X = State;
 		true ->
-			NewId = #id{name = State#state.id#id.name, hash = State#state.id#id.hash},
-			Succ = find_successor(State#state.id#id.name, NewId),
-			case Succ of
-				NewId -> {noreply, State};
-				_ -> put({finger, State#state.next}, Succ),
-				{noreply, NewState}
-			end
-			
-	end;
-handle_cast({add_key, Name, Key, Value}, State) -> io:format("add key: ~w ~n",[State#state.id]), 
-	add_key(Name, Key, Value),
-	{noreply, State};
-	
-handle_cast({get_value, Name, Key}, State) -> io:format("get key: ~w ~n",[State#state.id]), 
-	get_value(Name, Key),
-	{noreply, State};
-	
-handle_cast({del_key, Name, Key}, State) -> io:format("del key: ~w ~n",[State#state.id]), 
-	del_key(Name, Key),
-	{noreply, State};
-
-handle_cast(check_pred,  State) -> io:format("check_pred: ~w ~n",[State#state.id]),  
-	
-	{noreply, State};
-handle_cast({notify, Pred}, State) -> io:format("notify: ~w ~n",[State#state.id]),
-	case Pred of
-		nil -> State = State#state{pred = Pred};
-		_ ->  io:format("predecesor not nil")
+			X = get_server(TempSucc#id.name)
 	end,
-	{noreply, State}.
+	if X#state.pred /= nil ->
+		if X#state.pred == State#state.id ->
+			X2 = State;
+		true ->
+			X2 = get_server(X#state.pred#id.name)
+		end,
+		XHash = X2#state.id#id.hash,
+		if XHash < State#state.succ#id.hash, XHash > State#state.id#id.hash ->
+			State2 = State#state{succ = X2#state.id};
+		State#state.id#id.hash == State#state.succ#id.hash ->
+			State2 = State#state{succ = X2#state.id};
+		State#state.id#id.hash > State#state.succ#id.hash, State#state.succ#id.hash > XHash ->
+			State2 = State#state{succ = X2#state.id};
+		true ->
+			State2 = State
+		end;
+	true -> 
+		State2 = State,
+		io:format("succ.pred = nil~n")
+	end,
+	notify(State2#state.succ#id.name, State2#state.id),
+	{noreply, State2};
+
+handle_cast(fix_fingers, State) -> 
+	io:format("~w fix_fingers~n",[State#state.id]), 
+	{noreply, State};
+	
+
+
+
+handle_cast(check_pred,  State) -> 
+	io:format("~w check_pred~n",[State#state.id]),
+	if State#state.pred == nil; State#state.id == State#state.pred ->
+		State2 = State;
+		true ->
+			Status = is_pred_alive(State#state.pred#id.name),
+			case Status of
+			alive -> 
+				State2 = State;
+			death ->
+				State2 = State#state{pred = nil}
+			end
+	end,
+	{noreply, State2};
+
+handle_cast({notify,Pred}, State) -> 
+	if Pred /= nil, State#state.pred == nil ->
+		io:format("Pred is nil~n"),
+		Pred_id = #id{name=Pred#id.name, hash=Pred#id.hash},
+		StateNew = State#state{pred = Pred_id};
+	Pred /= nil, Pred#id.hash > State#state.pred#id.hash, Pred#id.hash < State#state.id#id.hash ->
+		io:format("New Pred found~n"),
+		Pred_id = #id{name=Pred#id.name, hash=Pred#id.hash},
+		StateNew = State#state{pred = Pred_id};
+	Pred /= nil, Pred#id.hash < State#state.pred#id.hash, State#state.pred#id.hash == State#state.id#id.hash ->
+		io:format("New Pred found~n"),
+		Pred_id = #id{name=Pred#id.name, hash=Pred#id.hash},
+		StateNew = State#state{pred = Pred_id};
+	Pred /= nil, Pred#id.hash < State#state.pred#id.hash, State#state.pred#id.hash > State#state.id#id.hash ->
+		io:format("New Pred found~n"),
+		Pred_id = #id{name=Pred#id.name, hash=Pred#id.hash},
+		StateNew = State#state{pred = Pred_id};
+	true -> 
+		io:format("No new pred found~n"),
+		StateNew = nil
+	end,
+	if StateNew /= nil ->
+		{noreply, StateNew};
+	true ->
+		{noreply, State}
+	end.
 
 %%--------------------------------------------------------------------
 %% Function: handle_info(Info, State) -> {noreply, State} |
@@ -272,15 +296,14 @@ code_change(_OldVsn, State, _Extra) ->
 %%--------------------------------------------------------------------
 %%% Internal functions
 %%--------------------------------------------------------------------
-find_succ(Proc, _Id, #state{id=#id{name=Me,hash=Hash}}) ->
-	io:format("find_succ~n"),
-	Id = #id{name=Me, hash=Hash},
-    Proc ! {find_successor, Id}.
-   
-    
-get_pre(Proc, _Id, State=#state{id=#id{name=_,hash=_}}) ->
-	io:format("find_pre~n"),
-    Proc ! {get_pred, State}.
+find_succ(Proc, _Id,#state{id=#id{name=Me,hash=Hash}}) ->
+    Proc ! {find_successor, #id{name=Me, hash=Hash}}.
+
+get_serv(Proc, State=#state{id=#id{name=_,hash=_}}) ->
+	Proc ! {get_server, State}.
+	
+check_alive_status(Proc, State) ->
+	Proc ! {alive, State}.
 
 pot2(0) -> 1;
 pot2(N) -> 2*pot2(N-1).
